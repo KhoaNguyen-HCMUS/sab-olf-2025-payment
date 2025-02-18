d75b01abf4c613c96da9b3afb284c41020038936
KhoaNguyen <anhkhoa515.dev@gmail.com>
2025-02-18 08:29:21 +0700

    Close form payment

--- src/components/App.js
+++ src/components/App.js
@@ -22,7 +22,16 @@ function App() {
           path='/'
           element={
             <div className='App-header'>
-              <h1>Thanh toán Hội thao OLYMFIT 2025</h1>
+              <h1>OLYMFIT 2025</h1>
+              <p>
+                Chào mừng đến với Hội thao OLYMFIT 2025! Các môn thi đấu đã đóng đơn đăng ký. Hãy sẵn sàng cổ vũ và tham
+                gia vào những trận đấu kịch tính!
+              </p>
+              <p>Tham gia group OLYMFIT 2025 để cập nhật thông tin mới nhất:</p>
+              <a href='https://t.sab.edu.vn/olf_community' target='_blank' rel='noopener noreferrer'>
+                Tham gia group{' '}
+              </a>
+              {/* <h1>Thanh toán Hội thao OLYMFIT 2025</h1>
               <div className='sport-buttons'>
                 <button className='sport-button badminton' onClick={() => handleSportClick('badminton')}>
                   Cầu Lông
@@ -30,13 +39,13 @@ function App() {
                 <button className='sport-button chess' onClick={() => handleSportClick('chess')}>
                   Cờ
                 </button>
-                {/* <button className='sport-button football' onClick={() => handleSportClick('football')}>
+                <button className='sport-button football' onClick={() => handleSportClick('football')}>
                   Bóng Đá
-                </button> */}
+                </button>
                 <button className='sport-button volleyball' onClick={() => handleSportClick('volleyball')}>
                   Bóng Chuyền
                 </button>
-              </div>
+              </div> */}
             </div>
           }
         />
